482e1120d3dfa2352ce4ea77a49329c2925d918c
CoderNinja100 <134537441+CoderNinja100@users.noreply.github.com>
2023-05-27 07:09:18 +0530

    Update Web_service.py

--- LP7/Web_service.py
+++ LP7/Web_service.py
@@ -3,9 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# The route() function of the Flask class is a decorator, which tells the application which URL should call the associated function.
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message='Hello, World!')
 
 if __name__ == '__main__':
+    # the run() method of Flask class runs the application on the local development server.
     app.run(debug=True)
